bf0c848be721067b516100638de15969d9a5a282
Jeydee Punsalang <60304750+jeydee123@users.noreply.github.com>
2022-12-13 20:28:03 -0800

    gh pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-\import './App.css';
+import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import WelcomePage from "./components/pages/WelcomePage"
 import QuestionPage from "./components/pages/QuestionPage"
